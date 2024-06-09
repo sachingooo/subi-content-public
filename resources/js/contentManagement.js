8eb5004ff0b3d712299fe9d99c2ea66aa53ade04
sachingooo <sachin.govind.too@gmail.com>
2024-06-09 12:02:26 -0500

    auth

--- resources/js/contentManagement.js
+++ resources/js/contentManagement.js
@@ -22,17 +22,35 @@ function hideLoader() {
 
 function promptForPasswords() {
 
-	sessionPass1 = prompt("Password 1", "");
+	const useCachedPasswords = confirm("Use cached passwords?");
+	if (useCachedPasswords) {
+		sessionPass1 = localStorage.getItem("sessionPass1");
+		sessionPass2 = localStorage.getItem("sessionPass2");
+	} else {
+		sessionPass1 = "";
+		sessionPass2 = "";
+	}
+
 	if (!sessionPass1) {
-		alert("You must enter a password to view this content.");
-		promptForPasswords();
+		sessionPass1 = prompt("Password 1", "");
+		if (!sessionPass1) {
+			alert("You must enter a password to view this content.");
+			promptForPasswords();
+		}
 	}
 
-	sessionPass2 = prompt("Password 2", "");
 	if (!sessionPass2) {
-		alert("You must enter a password to view this content.");
-		promptForPasswords();
+		sessionPass2 = prompt("Password 2", "");
+		if (!sessionPass2) {
+			alert("You must enter a password to view this content.");
+			promptForPasswords();
+		}
 	}
 
-	isAuthenticated = (sessionPass1 && sessionPass2);
+	localStorage.setItem("sessionPass1", sessionPass1);
+	localStorage.setItem("sessionPass2", sessionPass2);
+}
+
+function isAuthenticated() {
+	return (sessionPass1 && sessionPass2);
 }
